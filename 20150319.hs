4a1129abd5ede0c7ff8e9c3aeaa1c3b3f4179f4b
Leandro Farias <lferrazfarias@albany.edu>
2015-03-19 10:10:54 -0300

    novos exercicios

--- 20150319.hs
+++ 20150319.hs
@@ -32,3 +32,23 @@ sumPairs as bs
  | as == [] = bs
  | bs == [] = as
  | otherwise = (head as) + (head bs) : sumPairs (tail as) (tail bs)
+ 
+{-Bonus: Problema somar os elementos de uma lista de outro jeito-}
+sumList [] = 0
+sumList (a:as) = a + sumList as
+
+double2 [] = []
+double2 (a:as) = (2*a) : (double2 as)
+
+{-Exemplo casamento de padroes, selecionando uma lista de um so elemento-}
+double3 [] = []
+double3 (a:[]) = [2*a] --ou (2*a):[]
+
+--Quicksort
+
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort (a:[]) = [a]
+quicksort (a:as)
+ | a < (head as) = a : quicksort as
+ --| a > (head as) = ([(head as)] ++ [a]) : quicksort as
